f8c74ba13b846a38f1dfcbc0b4189ac8b1e8eb20
alyabogd <alyabogd@gmail.com>
2017-10-27 17:30:07 +0300

    Improve logging. Add debug mode.

---
+++ telegram_bot/checker.py
@@ -0,0 +1,14 @@
+"""This module provides functional for checking equality of text files"""
+
+
+def get_text(file_name):
+    with open(file_name, "r") as inp:
+        text = inp.readlines()
+    return text
+
+
+def files_equal(file_name_a, file_name_b):
+    text_a = get_text(file_name_a)
+    text_b = get_text(file_name_b)
+
+    return text_a == text_b
--- telegram_bot/conversation_bot.py
+++ telegram_bot/conversation_bot.py
@@ -7,6 +7,7 @@ from telegram import ChatAction
 from compression import decompress, compress
 from telegram_bot.configs import Config
 from telegram_bot.web_parser import get_links_to_text_files, download_text_file
+from telegram_bot.checker import files_equal
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -30,19 +31,24 @@ def _send_file(bot, chat_id, file_name):
     bot.send_document(chat_id=chat_id, document=open(file_name, "rb"))
 
 
-def _decompress(from_file):
-    to_file = "{}.txt".format(".".join(from_file.split(".")[:-1]))
+def _decompress(from_file, to_file=None):
+    if not to_file:
+        to_file = "{}.txt".format(".".join(from_file.split(".")[:-1]))
+    logging.debug("decompressing {} -> {}".format(from_file, to_file))
     decompress.main(from_file, to_file)
     return to_file
 
 
-def _compress(from_file):
-    to_file = "{}.in".format(".".join(from_file.split(".")[:-1]))
+def _compress(from_file, to_file=None):
+    if not to_file:
+        to_file = "{}.in".format(".".join(from_file.split(".")[:-1]))
+    logging.debug("compressing {} -> {}".format(from_file, to_file))
     compress.main(from_file, to_file)
     return to_file
 
 
 def files_handler(bot, update):
+    logging.info("Start processing file {}".format(update.message.document))
     bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.UPLOAD_DOCUMENT)
     in_file = _download_file(bot, update.message.document)
     update.message.reply_text(Config.FILE_UPLOADED_MESSAGE)
@@ -50,20 +56,30 @@ def files_handler(bot, update):
     file_type = in_file.split(".")[-1]
     if file_type in Config.compressed_types:
         update.message.reply_text(Config.DECOMPRESS_STARTED_MESSAGE.format(file_name=in_file))
-        logging.info("decompressing {}".format(in_file))
         out_file = _decompress(in_file)
     else:
         update.message.reply_text(Config.COMPRESS_STARTED_MESSAGE.format(file_name=in_file))
-        logging.info("compressing {}".format(in_file))
         out_file = _compress(in_file)
 
     update.message.reply_text(Config.DONE_MASSAGE)
-    logging.info("finish {}".format(in_file))
+    logging.info("processed {}".format(in_file))
+
     bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.UPLOAD_DOCUMENT)
     _send_file(bot, update.message.chat_id, out_file)
 
     os.remove(in_file)
     os.remove(out_file)
+
+
+def _check_compression(full_file, compressed_file):
+    logging.debug("Checking {} -> {}".format(full_file, compressed_file))
+    decompressed = _decompress(compressed_file, "check.txt")
+    if files_equal(full_file, "check.txt"):
+        logging.debug("correct")
+    else:
+        logging.error("error found {} -> {}".format(full_file, compressed_file))
+
+    os.remove(decompressed)
 
 
 def web_page_handler(bot, update, args):
@@ -72,10 +88,16 @@ def web_page_handler(bot, update, args):
         return
 
     web_address = args[0]
+
+    debug = False
+    if len(args) > 1 and args[1] == "debug":
+        debug = True
+        logging.info("-- scan with debug --")
+
     logging.info("Start parsing {}".format(web_address))
     text_links = get_links_to_text_files(web_address)
 
-    logging.info("Got {} links from {}".format(len(text_links), web_address))
+    logging.debug("Got {} links from {}".format(len(text_links), web_address))
     update.message.reply_text(Config.WEB_SCAN_RESULT.format(len(text_links)))
 
     for file in text_links:
@@ -84,15 +106,17 @@ def web_page_handler(bot, update, args):
         in_file = download_text_file(web_address, file)
 
         update.message.reply_text(Config.COMPRESS_STARTED_MESSAGE.format(file_name=in_file))
-        logging.info("compressing {}".format(in_file))
         out_file = _compress(in_file)
 
         update.message.reply_text(Config.DONE_MASSAGE)
-        logging.info("finish {}".format(in_file))
 
         bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.UPLOAD_DOCUMENT)
         _send_file(bot, update.message.chat_id, out_file)
 
+        if debug:
+            _check_compression(in_file, out_file)
+
+        logging.info("Done processing {}".format(file))
         os.remove(in_file)
         os.remove(out_file)
 
